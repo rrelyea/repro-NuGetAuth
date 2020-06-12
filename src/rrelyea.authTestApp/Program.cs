5d049019ac9454f981eddc49d62ad8b69d493e8b
Rob Relyea <rrelyea@microsoft.com>
2020-06-12 13:54:30 -0700

    use the packages

--- src/rrelyea.authTestApp/Program.cs
+++ src/rrelyea.authTestApp/Program.cs
@@ -7,6 +7,9 @@ namespace rrelyea.authTestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var usepackage1 = new classlib1.Class1();
+            var usepackage2 = new classlib2.Class1();
         }
     }
 }
